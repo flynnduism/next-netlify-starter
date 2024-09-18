9198298318b938d378e0dfb3963c59c1897f42ce
Ronan Flynn-Curran <dev@ronan.design>
2024-09-18 13:56:36 -0700

    text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>contact@distribute.systems</code>
         </p>
       </main>
 
